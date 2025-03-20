4f421c9c4fc3c82b2f33dbfa4bc6516265161617
movsesyan2002 <dav.movsesyan44@gmail.com>
2025-03-20 20:30:31 +0400

    Update task1.cs

--- task1.cs
+++ task1.cs
@@ -14,104 +14,104 @@
 // 4. Comparison Operators (>, <, >=, <=)
 // Compare accounts based on balance.
 
-// class BankAccount
-// {
-//     private int _Accountnumber;
-//     private decimal _Balance;
+class BankAccount
+{
+    private int _Accountnumber;
+    private decimal _Balance;
 
-//     public BankAccount(int accountnumber,decimal balance)
-//     {
-//         this._Accountnumber = accountnumber;
-//         this._Balance = balance;
-//     }
+    public BankAccount(int accountnumber,decimal balance)
+    {
+        this._Accountnumber = accountnumber;
+        this._Balance = balance;
+    }
 
-//     public int Accountnumber
-//     {
-//         get { return _Accountnumber; }
-//     }
+    public int Accountnumber
+    {
+        get { return _Accountnumber; }
+    }
 
-//     public decimal Balance
-//     {
-//         get { return _Balance; }
-//     }
+    public decimal Balance
+    {
+        get { return _Balance; }
+    }
 
-//     public override string ToString()
-//     {
-//         return $"Account: {this.Accountnumber}, Balance: {this.Balance} USD";
-//     }
+    public override string ToString()
+    {
+        return $"Account: {this.Accountnumber}, Balance: {this.Balance} USD";
+    }
 
-//     public override bool Equals(object obj)
-//     {
+    public override bool Equals(object obj)
+    {
 
-//         BankAccount other = (BankAccount)obj;
-//         if (other.Accountnumber == this.Accountnumber) {
-//             return true;
-//         }
+        BankAccount other = (BankAccount)obj;
+        if (other.Accountnumber == this.Accountnumber) {
+            return true;
+        }
 
-//         else {
-//             return false;
-//         }
+        else {
+            return false;
+        }
 
-//     }
+    }
 
-//     public override int GetHashCode()
-//     {
-//         return this.Accountnumber.GetHashCode() ^ 6;
-//     }
+    public override int GetHashCode()
+    {
+        return this.Accountnumber.GetHashCode() ^ 6;
+    }
 
-//     public static BankAccount operator +(BankAccount a,decimal b)
-//     {
-//         a._Balance += b;
-//         return a;
-//     }
+    public static BankAccount operator +(BankAccount a,decimal b)
+    {
+        a._Balance += b;
+        return a;
+    }
 
-//     public static BankAccount operator -(BankAccount a,decimal b)
-//     {
-//         if (b > a._Balance){
-//             Console.WriteLine("Balance is lower than your entered money");
-//             return a ;
-//         }
+    public static BankAccount operator -(BankAccount a,decimal b)
+    {
+        if (b > a._Balance){
+            Console.WriteLine("Balance is lower than your entered money");
+            return a ;
+        }
 
-//         else{
-//             a._Balance -= b;
-//             return a;
-//         }    
-//     }
+        else{
+            a._Balance -= b;
+            return a;
+        }    
+    }
 
-//     public static bool operator <(BankAccount a,BankAccount b)
-//     {
-//         return (a < b);
-//     }
-//     public static bool operator >(BankAccount a, BankAccount b)
-//     {
-//         return !(a < b);
-//     }
+    public static bool operator <(BankAccount a,BankAccount b)
+    {
+        return (a < b);
+    }
+    public static bool operator >(BankAccount a, BankAccount b)
+    {
+        return !(a < b);
+    }
 
-//     public static bool operator >=(BankAccount a, BankAccount b)
-//     {
-//         return (a >= b);
-//     }
+    public static bool operator >=(BankAccount a, BankAccount b)
+    {
+        return (a >= b);
+    }
 
-//     public static bool operator <=(BankAccount a, BankAccount b)
-//     {
-//         return !(a >= b);
-//     }
+    public static bool operator <=(BankAccount a, BankAccount b)
+    {
+        return !(a >= b);
+    }
 
-// }
+}
 
 
-// class Program
-// {
+class Program
+{
 
-//     static void Main(string[] args)
-//     {
-//         BankAccount account = new BankAccount(15,1500);
-//         Console.WriteLine(account.ToString());
-//         BankAccount account1 = new BankAccount(15,1600);
-//         Console.WriteLine($"first and second accounts are equal {account.Equals(account1)}");
-//         Console.WriteLine($"First account Balance is after summing balance {account + 1500}");
-//         Console.WriteLine($"");
-//         account = account + 5;
-//     }
-// }
+    static void Main(string[] args)
+    {
+        BankAccount account = new BankAccount(15,1500);
+        Console.WriteLine(account.ToString());
+        BankAccount account1 = new BankAccount(15,1600);
+        Console.WriteLine($"first and second accounts are equal {account.Equals(account1)}");
+        Console.WriteLine($"First account Balance is after summing balance {account + 1500}");
+        Console.WriteLine($"");
+        account = account + 5;
+    }
+}
 
