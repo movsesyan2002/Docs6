c7c78da3c776901df228ebc32ca20088ee8542e5
movsesyan2002 <dav.movsesyan44@gmail.com>
2025-03-20 20:31:02 +0400

    Update task2.cs

--- task2.cs
+++ task2.cs
@@ -20,139 +20,139 @@
 
 // 6. Overload >, <, >=, <= Based on Magnitude
 
-// using System.Numerics;
-// using System.Text;
+using System.Numerics;
+using System.Text;
 
-// class Vector3D
-// {
+class Vector3D
+{
    
-//     double X {get;}
-//     double Y {get;}
-//     double Z {get;}
+    double X {get;}
+    double Y {get;}
+    double Z {get;}
   
-//     public Vector3D(double x, double y, double z)
-//     {
-//         this.X = x;
-//         this.Y = y;
-//         this.Z = z;
-//     }
+    public Vector3D(double x, double y, double z)
+    {
+        this.X = x;
+        this.Y = y;
+        this.Z = z;
+    }
 
 
-//     public override string ToString()
-//     {
-//         return $"X parameter is {this.X}\nY parameter is {this.Y}\nZ parameter is {this.Z}";
-//     }
+    public override string ToString()
+    {
+        return $"X parameter is {this.X}\nY parameter is {this.Y}\nZ parameter is {this.Z}";
+    }
 
-//     public static Vector3D operator +(Vector3D v, Vector3D v2)
-//     {
-//         return new Vector3D(v.X  + v2.X, v.Y + v2.Y, v.Z + v2.Z);
-//     }
+    public static Vector3D operator +(Vector3D v, Vector3D v2)
+    {
+        return new Vector3D(v.X  + v2.X, v.Y + v2.Y, v.Z + v2.Z);
+    }
 
-//     public static Vector3D operator -(Vector3D v, Vector3D v2)
-//     {
-//         return new Vector3D(v.X  - v2.X, v.Y - v2.Y, v.Z - v2.Z);
-//     }
+    public static Vector3D operator -(Vector3D v, Vector3D v2)
+    {
+        return new Vector3D(v.X  - v2.X, v.Y - v2.Y, v.Z - v2.Z);
+    }
 
-//     public static Vector3D operator *(Vector3D v, Vector3D v2)
-//     {
-//         return new Vector3D(v.X * v2.X, v.Y * v2.Y, v.Z * v2.Z);
-//     }
+    public static Vector3D operator *(Vector3D v, Vector3D v2)
+    {
+        return new Vector3D(v.X * v2.X, v.Y * v2.Y, v.Z * v2.Z);
+    }
 
-//     public static Vector3D operator /(Vector3D v, Vector3D v2)
-//     {
-//         if (v2.X == 0 || v2.Y == 0 || v2.Z == 0)
-//         {
-//             throw new ArgumentException("One of this is zero");
-//         }
-//         else
-//         {
-//             return new Vector3D(v.X / v2.X, v.Y / v2.Y, v.Z / v2.Z);
-//         } 
-//     }
+    public static Vector3D operator /(Vector3D v, Vector3D v2)
+    {
+        if (v2.X == 0 || v2.Y == 0 || v2.Z == 0)
+        {
+            throw new ArgumentException("One of this is zero");
+        }
+        else
+        {
+            return new Vector3D(v.X / v2.X, v.Y / v2.Y, v.Z / v2.Z);
+        } 
+    }
 
-//     public static bool operator ==(Vector3D v, Vector3D v2)
-//     {
-//         return v.X == v2.X && v.Y == v2.Y && v.Z == v2.Z;
-//     }
+    public static bool operator ==(Vector3D v, Vector3D v2)
+    {
+        return v.X == v2.X && v.Y == v2.Y && v.Z == v2.Z;
+    }
 
-//     public static bool operator !=(Vector3D v, Vector3D v2)
-//     {
-//         return !(v == v2);
-//     }
+    public static bool operator !=(Vector3D v, Vector3D v2)
+    {
+        return !(v == v2);
+    }
 
-//     public override bool Equals(object? obj)
-//     {
-//         return base.Equals(obj);
-//     }
+    public override bool Equals(object? obj)
+    {
+        return base.Equals(obj);
+    }
 
-//     public override int GetHashCode()
-//     {
-//         return base.GetHashCode();
-//     }
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
 
-//     public double Magnitude() {
-//         return Math.Sqrt(X * X + Y * Y + Z * Z);
-//     }
+    public double Magnitude() {
+        return Math.Sqrt(X * X + Y * Y + Z * Z);
+    }
 
-//     public static bool operator true(Vector3D v1)
-//     {
-//         return v1.Magnitude() > 0;
-//     }
+    public static bool operator true(Vector3D v1)
+    {
+        return v1.Magnitude() > 0;
+    }
 
-//     public static bool operator false(Vector3D v1)
-//     {
-//        return v1.Magnitude() < 0;
-//     }
+    public static bool operator false(Vector3D v1)
+    {
+       return v1.Magnitude() < 0;
+    }
 
-//     public static bool operator <(Vector3D v1, Vector3D v2)
-//     {
-//         return (v1.X + v1.Y + v1.Z) < (v2.X + v2.Y + v2.Z);
-//     }
+    public static bool operator <(Vector3D v1, Vector3D v2)
+    {
+        return (v1.X + v1.Y + v1.Z) < (v2.X + v2.Y + v2.Z);
+    }
 
-//     public static bool operator >(Vector3D v1, Vector3D v2)
-//     {
-//         return (v1.X + v1.Y + v1.Z) > (v2.X + v2.Y + v2.Z);
-//     }
+    public static bool operator >(Vector3D v1, Vector3D v2)
+    {
+        return (v1.X + v1.Y + v1.Z) > (v2.X + v2.Y + v2.Z);
+    }
 
-//     public static bool operator <=(Vector3D v1, Vector3D v2)
-//     {
-//         return (v1.X + v1.Y + v1.Z) <= (v2.X + v2.Y + v2.Z);
-//     }
+    public static bool operator <=(Vector3D v1, Vector3D v2)
+    {
+        return (v1.X + v1.Y + v1.Z) <= (v2.X + v2.Y + v2.Z);
+    }
 
-//     public static bool operator >=(Vector3D v1, Vector3D v2)
-//     {
-//         return (v1.X + v1.Y + v1.Z) >= (v2.X + v2.Y + v2.Z);
-//     }
+    public static bool operator >=(Vector3D v1, Vector3D v2)
+    {
+        return (v1.X + v1.Y + v1.Z) >= (v2.X + v2.Y + v2.Z);
+    }
 
 
-// }
+}
 
-// class Program
-// {
-//     static void Main(string[] args)
-//     {
-//         Vector3D v1 = new Vector3D(4.0, 5.0, 8.0);
-//         Vector3D v2 = new Vector3D(3.0, 4.0, 2.0);
+class Program
+{
+    static void Main(string[] args)
+    {
+        Vector3D v1 = new Vector3D(4.0, 5.0, 8.0);
+        Vector3D v2 = new Vector3D(3.0, 4.0, 2.0);
 
-//         Console.WriteLine($"First vector is\n{v1.ToString()}");
-//         Console.WriteLine($"Second vector is\n{v2.ToString()}");
-//         Vector3D v3 = v1 + v2;
-//         Console.WriteLine($"Sum of vectors is\n{v3.ToString()}");
-//         v3 = v1 - v2;
-//         Console.WriteLine($"Subtractin of vectors is\n{v3.ToString()}");
-//         v3 = v1 * v2;
-//         Console.WriteLine($"Multiply of vectors is\n{v3.ToString()}");
-//         v3 = v1 / v2;
-//         Console.WriteLine($"Divide of vectors is\n{v3.ToString()}");
-//         Console.WriteLine($"Object are equal {v1.Equals(v2)}");
-//         if (v1) 
-//         {
-//             Console.WriteLine("Vector is true");
-//         }
-//         Console.WriteLine($"First vector is < than Second {v1 < v2}");
-//         Console.WriteLine($"First vector is > than Second {v1 > v2}");
-//         Console.WriteLine($"First vector is <= than Second {v1 <= v2}");
-//         Console.WriteLine($"First vector is >= than Second {v1 >= v2}");
+        Console.WriteLine($"First vector is\n{v1.ToString()}");
+        Console.WriteLine($"Second vector is\n{v2.ToString()}");
+        Vector3D v3 = v1 + v2;
+        Console.WriteLine($"Sum of vectors is\n{v3.ToString()}");
+        v3 = v1 - v2;
+        Console.WriteLine($"Subtractin of vectors is\n{v3.ToString()}");
+        v3 = v1 * v2;
+        Console.WriteLine($"Multiply of vectors is\n{v3.ToString()}");
+        v3 = v1 / v2;
+        Console.WriteLine($"Divide of vectors is\n{v3.ToString()}");
+        Console.WriteLine($"Object are equal {v1.Equals(v2)}");
+        if (v1) 
+        {
+            Console.WriteLine("Vector is true");
+        }
+        Console.WriteLine($"First vector is < than Second {v1 < v2}");
+        Console.WriteLine($"First vector is > than Second {v1 > v2}");
+        Console.WriteLine($"First vector is <= than Second {v1 <= v2}");
+        Console.WriteLine($"First vector is >= than Second {v1 >= v2}");
 
-//     }
-// }
+    }
+}
